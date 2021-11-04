abefde390bf2cf12e74c3c27cec49c226c389803
nguy11 <duc11.nguyenvan@nttdata.com>
2021-11-04 10:00:30 +0700

    Correct topic and consumer

--- kafka/kafka-python-client/client-consumer.py
+++ kafka/kafka-python-client/client-consumer.py
@@ -1,18 +1,18 @@
+# Doc: https://docs.confluent.io/clients-confluent-kafka-python/current/overview.html
+
 from confluent_kafka import Consumer
 
 conf = {'bootstrap.servers': 'localhost:9092',
-        'group.id': 'group'}
+        'group.id': 'test-group'}
 
 consumer = Consumer(conf)
 
 try:
-    # consumer.subscribe('topic1')
+    consumer.subscribe(["test-topic"])
 
     while True:
         msg = consumer.poll(timeout=1.0)
         print(msg)
-        # if msg is None:
-        #     continue
 
 finally:
     consumer.close()
--- kafka/kafka-python-client/client-producer.py
+++ kafka/kafka-python-client/client-producer.py
@@ -3,15 +3,10 @@
 from confluent_kafka import Producer
 
 producer = Producer({'bootstrap.servers': 'localhost:9092'})
-msg = ('kafka_test' * 20).encode()[:100]
-print(msg)
+msg = 'test'
 size = 10
 
-#
-# def delivery_report(err, decoded_message, original_message):
-#     if err is not None:
-#         print(err)
 for _ in range(size):
-    producer.produce('topic1', msg)
+    producer.produce('test-topic', msg)
 
 producer.flush()
